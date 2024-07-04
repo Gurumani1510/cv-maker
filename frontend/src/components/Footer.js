712d4e35fec2c9d124d9a48598036688430af58c
Gurumani1510 <121172274+Gurumani1510@users.noreply.github.com>
2024-07-04 18:07:48 +0530

    Add files via upload

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+
+function Footer() {
+  return (
+    <Container fluid  className="border-top mt-2 text-center bg-light text-dark py-3">
+    </Container>
+  )
+}
+
+export default Footer
